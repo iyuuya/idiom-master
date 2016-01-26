3be9f69513fb14d5e28cba213b7653c8cb35d170
iyuuya <i.yuuya@gmail.com>
2016-01-26 13:52:28 +0900

    fix json to csv

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -1,14 +1,9 @@
 class Extractor
   class << self
     def extract
-      File.open(Rails.root.join('db/fixtures/characters.json'), 'w') do |f|
-        characters = Character.all
-        f.write characters.to_json
-      end
-
-      File.open(Rails.root.join('db/fixtures/three_phrases.json'), 'w') do |f|
+      File.open(Rails.root.join('db/fixtures/three_phrases.csv'), 'w') do |f|
         three_phrases = ThreePhrase.all
-        f.write three_phrases.to_json
+        f.write three_phrases.map(&:to_s).sort.join("\n")
       end
     end
   end
