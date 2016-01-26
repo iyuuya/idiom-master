83e45fe9a5fcd2205a43e9c05622542b450e8bee
iyuuya <i.yuuya@gmail.com>
2016-01-26 23:26:31 +0900

    add four phrases extract

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -5,6 +5,11 @@ class Extractor
         three_phrases = ThreePhrase.all
         f.write three_phrases.map(&:to_s).sort.join("\n")
       end
+
+      File.open(Rails.root.join('db/fixtures/four_phrases.csv'), 'w') do |f|
+        four_phrases = FourPhrase.all
+        f.write four_phrases.map(&:to_s).sort.join("\n")
+      end
     end
   end
 end
