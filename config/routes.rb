eec44074d0c94596ce8a685d75adf19fd635e7e9
iyuuya <i.yuuya@gmail.com>
2016-01-26 15:44:29 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :four_phrases
   root to: 'three_phrases#index'
 
   resources :three_phrases, except: [:edit, :update]
+  resources :four_phrases,  except: [:edit, :update]
 end
